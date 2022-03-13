59816acc4e63e176135145f3e22e9e585e5904ec
NatNyt <70100268+NatNyt@users.noreply.github.com>
2022-03-13 16:00:47 +0700

    Update

--- TruewalletVoucher.py
+++ TruewalletVoucher.py
@@ -9,7 +9,7 @@ async def twVoucher(tel, code = str, _callback = None):
     code = list(filter(None, code))
     if  not len(code[0]) == 18:
         raise Exception("Invalid Voucher")
-    res =  requests.post('https://gift.truemoney.com/campaign/vouchers/' + code[0] + '/redeem', data=json.dumps({'mobile': tel, 'voucher_hash': code[0]}), headers={'Content-Type': 'application/json'}).json()
+    res =  requests.post('https://voucher.meowcdn.xyz/api', data=json.dumps({'mobile': tel, 'voucher': code[0]}), headers={'Content-Type': 'application/json'}).json()
     print(str(res.get("status")["code"]).lower().replace('\n', '').replace(' ', ''))
     if str(res.get("status")["code"]).lower().replace('\n', '').replace(' ', '') == 'success':
         _callback(res.get("data")["voucher"]["redeemed_amount_baht"], res.get("data")["owner_profile"]["full_name"], code[0])
