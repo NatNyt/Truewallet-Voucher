4dfaad753c00e5c4028810e0f38d74e9152c9235
NatNyt <70100268+NatNyt@users.noreply.github.com>
2022-03-17 22:25:20 +0700

    Bug Fix

--- TruewalletVoucher.py
+++ TruewalletVoucher.py
@@ -9,9 +9,16 @@ async def twVoucher(tel, code = str, _callback = None):
     code = list(filter(None, code))
     if  not len(code[0]) == 18:
         raise Exception("Invalid Voucher")
-    res =  requests.post('https://voucher.meowcdn.xyz/api', data=json.dumps({'mobile': tel, 'voucher': code[0]}), headers={'Content-Type': 'application/json'}).json()
-    print(str(res.get("status")["code"]).lower().replace('\n', '').replace(' ', ''))
-    if str(res.get("status")["code"]).lower().replace('\n', '').replace(' ', '') == 'success':
-        _callback(res.get("data")["voucher"]["redeemed_amount_baht"], res.get("data")["owner_profile"]["full_name"], code[0])
+    vertify = requests.get('https://gift.truemoney.com/campaign/vouchers/{code[0]}/verify', headers={'Content-Type': 'application/json'}).json()
+    if vertify.get('status')["code"] == "VOUCHER_NOT_FOUND" or vertify.get('status')["code"] == "VOUCHER_OUT_OF_STOCK":
+        raise Exception(vertify.get('status')["message"])
     else:
-        raise Exception("Invalid Voucher : " + code[0])
+        vertify_phone = requests.get('https://gift.truemoney.com/campaign/vouchers/{code[0]}/verify?mobile={tel}', headers={'Content-Type': 'application/json'}).json()
+        if vertify_phone.get("status")["code"] == "SUCCESS":
+            res =  requests.post('https://gift.truemoney.com/campaign/vouchers/' + code[0] + '/redeem', data=json.dumps({'mobile': tel, 'voucher_hash': code[0]}), headers={'Content-Type': 'application/json'}).json()
+            if str(res.get("status")["code"]).lower().replace('\n', '').replace(' ', '') == 'success':
+                _callback(res.get("data")["voucher"]["redeemed_amount_baht"], res.get("data")["owner_profile"]["full_name"], code[0])
+            else:
+                raise Exception("Invalid Voucher : " + code[0])
+        else:
+            raise Exception("Invalid Phone number")
