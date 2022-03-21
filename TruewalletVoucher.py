c7d021eb9c8c60888e509d758f170829ca8fb98d
NatNyt <70100268+NatNyt@users.noreply.github.com>
2022-03-21 18:06:23 +0700

    Bug Fix

--- TruewalletVoucher.py
+++ TruewalletVoucher.py
@@ -9,16 +9,8 @@ async def twVoucher(tel, code = str, _callback = None):
     code = list(filter(None, code))
     if  not len(code[0]) == 18:
         raise Exception("Invalid Voucher")
-    vertify = requests.get('https://gift.truemoney.com/campaign/vouchers/{code[0]}/verify', headers={'Content-Type': 'application/json'}).json()
-    if vertify.get('status')["code"] == "VOUCHER_NOT_FOUND" or vertify.get('status')["code"] == "VOUCHER_OUT_OF_STOCK":
-        raise Exception(vertify.get('status')["message"])
+    res =  requests.post('https://fresh-shop.xyz/api/truewallet/voucher', data=json.dumps({'phone': tel, 'voucher': code[0]}), headers={'Content-Type': 'application/json'}).json()
+    if str(res.get("data")["status"]["code"]).lower().replace('\n', '').replace(' ', '') == 'success':
+        _callback(res.get("data")["data"]["voucher"]["redeemed_amount_baht"], res.get("data")["data"]["owner_profile"]["full_name"], code[0])
     else:
-        vertify_phone = requests.get('https://gift.truemoney.com/campaign/vouchers/{code[0]}/verify?mobile={tel}', headers={'Content-Type': 'application/json'}).json()
-        if vertify_phone.get("status")["code"] == "SUCCESS":
-            res =  requests.post('https://gift.truemoney.com/campaign/vouchers/' + code[0] + '/redeem', data=json.dumps({'mobile': tel, 'voucher_hash': code[0]}), headers={'Content-Type': 'application/json'}).json()
-            if str(res.get("status")["code"]).lower().replace('\n', '').replace(' ', '') == 'success':
-                _callback(res.get("data")["voucher"]["redeemed_amount_baht"], res.get("data")["owner_profile"]["full_name"], code[0])
-            else:
-                raise Exception("Invalid Voucher : " + code[0])
-        else:
-            raise Exception("Invalid Phone number")
+        raise Exception("Invalid Voucher")
